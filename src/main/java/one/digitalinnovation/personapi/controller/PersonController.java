cdcd3150d81ec5f0839a5a264081c230b9b8e2d7
Wesley-yuri <wesley_analista@outlook.com>
2022-01-13 22:37:19 -0300

    Created README.md file

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,6 +1,7 @@
 package one.digitalinnovation.personapi.controller;
 
 
+import lombok.AllArgsConstructor;
 import one.digitalinnovation.personapi.Exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
@@ -13,14 +14,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
 
     private PersonService personService;
-
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
